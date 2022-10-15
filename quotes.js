8cd532e74d7e9c8226afbe87bf68b4cbe091f60a
cooldued05 <85801567+cooldued05@users.noreply.github.com>
2022-10-15 12:24:33 +0100

    no
    no

---
+++ quotes.js
@@ -0,0 +1,23 @@
+const localQuotes = [
+{
+    
+    text: "quote1",
+    auther: "unkown1"
+},
+  {
+    text: "quote2",
+    auther: "unkown2"
+},
+    {
+    text: "quote3",
+    auther: "unkown3"
+},
+    {
+    text: "quote4",
+    auther: "unkown4"
+},
+    {
+    text: "quote5",
+    auther: "unkown5"
+}
+    ]
