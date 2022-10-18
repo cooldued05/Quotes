3b63bb5ef73a24df04fc1284fb9f01cebead1744
cooldued05 <85801567+cooldued05@users.noreply.github.com>
2022-10-18 12:54:57 +0100

    Update quotes.js

--- quotes.js
+++ quotes.js
@@ -1,8 +1,8 @@
 [
 {
     
-    "text": "quote1",
-    "author": "unkown1"
+    "text": "The greatest glory in living lies not in never falling, but in rising every time we fall",
+    "author": "Nelson Mandela"
 },
   {
     "text": "quote2",
@@ -21,3 +21,10 @@
     "author": "unkown5"
 }
     ]
+The greatest glory in living lies not in never falling, but in rising every time we fall. -Nelson Mandela
+The way to get started is to quit talking and begin doing. -Walt Disney
+Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma – which is living with the results of other people's thinking. -Steve Jobs
+If life were predictable it would cease to be life, and be without flavor. -Eleanor Roosevelt
+If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough. -Oprah Winfrey
+If you set your goals ridiculously high and it's a failure, you will fail above everyone else's success. -James Cameron
+Life is what happens when you're busy making other plans. -John Lennon
